d36d9aec91cc3d8648a03b0e7a2e58fe1677bf75
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-06-04 20:04:11 -0300

    feat: create Person DTO

---
+++ src/main/java/com/levi/dto/request/PersonRequestDTO.java
@@ -0,0 +1,10 @@
+package com.levi.dto.request;
+
+public class PersonRequestDTO {
+
+    private String name;
+
+    private String cpf;
+
+    private Integer age;
+}
---
+++ src/main/java/com/levi/dto/response/PersonResponseDTO.java
@@ -0,0 +1,12 @@
+package com.levi.dto.response;
+
+public class PersonResponseDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String cpf;
+
+    private Integer age;
+}
