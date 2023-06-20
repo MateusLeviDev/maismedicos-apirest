4976bf7c62dba1f6009ef45a682c4d3d7e943a9d
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-06-20 19:18:50 -0300

    chore: implement `update` @PUT endpoint and @GET

--- src/main/java/com/levi/controller/MedicoController.java
+++ src/main/java/com/levi/controller/MedicoController.java
@@ -1,13 +1,11 @@
 package com.levi.controller;
 
-import com.levi.medico.DadosCadastroMedico;
-import com.levi.medico.DadosListagemMedico;
-import com.levi.medico.Medico;
-import com.levi.medico.MedicoRepository;
+import com.levi.medico.*;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,7 +23,13 @@ public class MedicoController {
 
     //page give the pageable information. list and infos
     @GetMapping
-    public Page<DadosListagemMedico> listar(Pageable pageable) {
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable) {
         return medicoRepository.findAll(pageable).map(DadosListagemMedico::new);
+    }
+
+    @PutMapping
+    public void atualizar(@RequestBody @Valid DadosAtualizarMedico dados) {
+        var medico = medicoRepository.getReferenceById(dados.id()); //load med by id (DadosAtualizarMedico)
+        medico.atualizarInformacoes(dados);
     }
 }
