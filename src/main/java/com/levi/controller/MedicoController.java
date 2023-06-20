f833fcb04b2e07277d55b7cd2c2639f4f9f34ec6
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-06-20 18:03:55 -0300

    chore: implementing @GetMapping List

--- src/main/java/com/levi/controller/MedicoController.java
+++ src/main/java/com/levi/controller/MedicoController.java
@@ -1,14 +1,16 @@
 package com.levi.controller;
 
 import com.levi.medico.DadosCadastroMedico;
+import com.levi.medico.DadosListagemMedico;
 import com.levi.medico.Medico;
 import com.levi.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/medicos")
@@ -17,7 +19,13 @@ public class MedicoController {
     @Autowired
     private MedicoRepository medicoRepository;
     @PostMapping
-    public void create(@RequestBody @Valid DadosCadastroMedico dadosCadastroMedico) {
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dadosCadastroMedico) {
         medicoRepository.save(new Medico(dadosCadastroMedico));
+    }
+
+    //page give the pageable information. list and infos
+    @GetMapping
+    public Page<DadosListagemMedico> listar(Pageable pageable) {
+        return medicoRepository.findAll(pageable).map(DadosListagemMedico::new);
     }
 }
