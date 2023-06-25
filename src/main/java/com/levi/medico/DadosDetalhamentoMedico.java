5de941ecb6a8b1111bfdd306c7eb0c3746e45e5f
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-06-25 00:10:25 -0300

    feat: create DadosDetalhamentoMedico.java DTO

--- src/main/java/com/levi/medico/DadosDetalhamentoMedico.java
+++ src/main/java/com/levi/medico/DadosDetalhamentoMedico.java
@@ -5,6 +5,6 @@ import com.levi.address.Address;
 public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, Specialty specialty, Address address) {
 
     public DadosDetalhamentoMedico(Medico medico) {
-        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getSpecialty(), medico.getAddress())
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getSpecialty(), medico.getAddress());
     }
 }
