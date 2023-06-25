7f6ce2ef8a4bea398bde625ee08ed1f1b54ec201
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-06-25 14:18:46 -0300

    create: Usuario @Entity

---
+++ src/main/java/com/levi/domain/usuario/Usuario.java
@@ -0,0 +1,22 @@
+package com.levi.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "usuario")
+@Table(name = "uuario")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
