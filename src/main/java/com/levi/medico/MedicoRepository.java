c427a1fe8e459daae1c059c1233ebf41ff41d685
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-06-25 00:09:38 -0300

    chore: create new method "findAllByAtivo"

--- src/main/java/com/levi/medico/MedicoRepository.java
+++ src/main/java/com/levi/medico/MedicoRepository.java
@@ -1,9 +1,15 @@
 package com.levi.medico;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.stream.DoubleStream;
 
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     //passa dois tipos de objetos. o tipo da entity que o repo vai trabalhar e o 2 o tipo de atributo PK
+
+    Page<Medico> findAllByAtivoTrue(Pageable pageable);
 }
